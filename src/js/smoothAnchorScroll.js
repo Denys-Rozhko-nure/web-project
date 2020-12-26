8c8281886374539d3c6eafee4cea7ce6595194e1
= <=>
2020-12-26 18:17:43 +0200

    Ended project functionality

--- src/js/smoothAnchorScroll.js
+++ src/js/smoothAnchorScroll.js
@@ -1,10 +1,14 @@
 document.querySelectorAll('a[href*="#"]').forEach(anchor => {
     anchor.addEventListener('click', e => {
         e.preventDefault();
-        
+
+        console.log("new scroll");
+        const el = document.getElementById(anchor.getAttribute('href').substr(1));
+        const header = document.querySelector("header");
+
         document.getElementById(anchor.getAttribute('href').substr(1)).scrollIntoView({
           behavior: 'smooth',
-          block: 'start'
+          top: el.getBoundingClientRect().top + window.pageYOffset - header.clientHeight
         });
     })
 })
--- src/script.js
+++ src/script.js
@@ -21,18 +21,15 @@ document.getElementById("settings-image").addEventListener("click", () => {
         
         settings.style.height = "auto";
         const clientHeight = settings.clientHeight;
-        console.log(clientHeight);
-        
-        settings.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
+        settings.style.height = "0px";
+        const pageYOffset1 = window.pageYOffset; 
 
         animate({
             timing: t => t,
-            duration: 1000,
+            duration: 1000, 
             draw(progress) {
                 settings.style.height = clientHeight * progress + "px";
+                window.scrollTo(0, pageYOffset1 * (1 - progress));
             }
         });
     } else {
@@ -42,7 +39,7 @@ document.getElementById("settings-image").addEventListener("click", () => {
             timing: t => t,
             duration: 1000,
             draw(progress) {
-                settings.style.height = Math.round(clientHeight *(1 - progress)) + "px";
+                settings.style.height = clientHeight *(1 - progress) + "px";
                 window.scrollTo(0, startYOffset - clientHeight * progress);
             }
         });
@@ -81,3 +78,32 @@ authoriseForm.addEventListener("submit", function(e) {
 
     setupAuthorised();
 });
+
+document.getElementById("menu-button").addEventListener("click", () => {
+    const nav = document.querySelector("nav");
+
+    if(window.getComputedStyle(nav).height == "0px") {
+        nav.style.height = "auto";
+        const clientHeight = nav.clientHeight;
+        nav.style.height = "0px";
+
+        animate({
+            timing: t => t * t,
+            duration: 200, 
+            draw(progress) {
+                nav.style.height = clientHeight * progress + "px";
+            }
+        });
+    } else {
+        const clientHeight = nav.clientHeight;
+        const startYOffset = window.pageYOffset;
+        animate({
+            timing: t => t * t,
+            duration: 200,
+            draw(progress) {
+                nav.style.height = clientHeight *(1 - progress) + "px";
+                window.scrollTo(0, startYOffset - clientHeight * progress);
+            }
+        });
+    }
+})
